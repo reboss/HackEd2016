b9dd2346a22fea011f91540ed13755270e2a1b91
John Robbins <j.reboss@gmail.com>
2016-11-12 12:43:58 -0700

    Add android project

---
+++ Android/RadarDetector/app/src/main/java/com/vortecs/reboss/radardetector/MainActivity.java
@@ -0,0 +1,22 @@
+package com.vortecs.reboss.radardetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setPhotoRadar(View view) {
+
+    }
+
+    public void setAccident(View view) {
+
+    }
+}
