4a35952b1fbf0d114ed3279c7b0854080e2c556d
John Robbins <j.reboss@gmail.com>
2016-11-12 22:31:41 -0700

    Add socketio code for initial connection to server

--- Android/RadarDetector/app/src/main/java/com/vortecs/reboss/radardetector/MainActivity.java
+++ Android/RadarDetector/app/src/main/java/com/vortecs/reboss/radardetector/MainActivity.java
@@ -3,19 +3,17 @@ package com.vortecs.reboss.radardetector;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.drivewyze.EventReceiver;
 import com.drivewyze.GPXLocationProvider;
 import com.drivewyze.JDRIVE;
-import com.drivewyze.LocationListener;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
@@ -23,28 +21,23 @@ import com.github.nkzawa.socketio.client.Socket;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String SERVER = "109.199.127.236";
+    private final String SERVER = "http://hacked2016.herokuapp.com";
     private static final String TAG = MainActivity.class.getSimpleName();
     private GPXLocationProvider gpxLocation;
-    private Location currentBestLocation = null;
+    private final double ABOUT_ONE_KILOMETER = 0.0085;
 
     private Socket mSocket;
 
     {
         try {
-            mSocket = IO.socket("http://chat.socket.io");
+            mSocket = IO.socket(SERVER);
         } catch (URISyntaxException e) {
         }
     }
@@ -80,15 +73,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String GetTheOSUData() {
+
+        // read osreplica.json, check for timed out
+        // readd timed out object with delete flag set to true
         return new String();
     }
 
-    public void setPhotoRadar(View view) throws IOException, JSONException {
-        pushToServer("PhotoRadar");
+    public void setPhotoRadar(View view) {
+        //double[]coordinates = getCoordinates();
+//        final double[] coordinates = {49.202011, 113.020292};
+//
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    pushToServer("Photoradar");
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//        thread.start();
+//        while (thread.isAlive()){}
+        Toast.makeText(getApplicationContext(), "Posted message, Thanks for being a good samaritan",
+                Toast.LENGTH_LONG).show();
     }
 
-    public void setAccident(View view) throws IOException, JSONException {
-        pushToServer("Acciddent");
+    public void setAccident(View view) {
+        //double[]coordinates = getCoordinates();
+        final double[] coordinates = {49.202011, 113.020292};
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pushToServer("Accident");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        while (thread.isAlive()){}
+        Toast.makeText(getApplicationContext(), "Posted message, Thanks for being a good samaritan",
+                Toast.LENGTH_LONG).show();
+
     }
 
     private double[] getCoordinates() {
@@ -103,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
 
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER, 5000, 10,
-                (android.location.LocationListener) locationListener);
+                (LocationListener) locationListener);
 
         double[] coord = {myLocation.getLat(), myLocation.getLong()};
         return coord;
@@ -115,43 +149,33 @@ public class MainActivity extends AppCompatActivity {
         JSONObject json = makeJSONObject(1, 1, type);
         JSONObject location = makeLocation();
 
-        String post = json.toString();
-        URL server = new URL(SERVER);
-        HttpURLConnection connection = (HttpURLConnection) server.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setFixedLengthStreamingMode(post.getBytes().length);
-        connection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
-        connection.connect();
+        json.put("location", location);
 
-        DataOutputStream reqStream = new DataOutputStream(connection.getOutputStream());
-        reqStream.writeBytes(post);
-        reqStream.flush();
-
-        Toast.makeText(getApplicationContext(), "Posted message, Thanks for being a good samaritan",
-                Toast.LENGTH_LONG).show();
+        mSocket.emit("new data", json);
     }
 
-    // Device details??
-    public void setDeviceDetails(String details) {
 
-        JDRIVE.instance().addListenerForEvent("myFenceEnterListener", "fence-enter", new EventReceiver() {
-            @Override
-            public void receive(String event, String ts) {
-                System.out.println("fence has been entered");
-            }
-        });
-
-        // how to remove fences??
-        JDRIVE.instance().osr(new EventReceiver() {
-            @Override
-            public void receive(String event, String ts) {
-                // get the fence and NLR objects from your backend or local store etc.
-                String payload = GetTheOSUData();
-                JDRIVE.instance().osu(payload, ts);
-            }
-        });
-    }
+//    public void setDeviceDetails(String details) {
+//
+//
+//        JDRIVE.instance().addListenerForEvent("myFenceEnterListener", "fence-enter", new EventReceiver() {
+//            @Override
+//            public void receive(String event, String ts) {
+//                // notify user about photo radar
+//                // UI event?
+//            }
+//        });
+//
+//        // how to remove fences??
+//        JDRIVE.instance().osr(new EventReceiver() {
+//            @Override
+//            public void receive(String event, String ts) {
+//                // get the fence and NLR objects from your backend or local store etc.
+//                String payload = GetTheOSUData();
+//                JDRIVE.instance().osu(payload, ts);
+//            }
+//        });
+//    }
 
 
     JSONObject makeJSONObject(int id, int _id, String type) throws JSONException {
